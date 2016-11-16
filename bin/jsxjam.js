ce5440daf5b004ce79c817f26b25688d84842f5a
Kevin Kirchner <kirchner.kevin@gmail.com>
2016-11-16 16:02:55 -0500

    writing React Component files from a json! :tada:

--- bin/jsxjam.js
+++ bin/jsxjam.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-var fs = require('fs')
+var fsPath = require('fs-path')
 var path = require('path')
 var jsonfile = require('jsonfile')
 var program = require('commander')
@@ -7,15 +7,58 @@ var program = require('commander')
 program
     .version('0.1.0')
     .option('-i, --input [path]', 'path to the json file to parse. Defaults to jsxjam.json')
-    .option('-o, --output [path]', 'directory where you want your generated jsx files to end up. Defaults to ./')
+    .option('-o, --output [path]', 'directory where the generated JSX files end up. Defaults to ./')
+    .option('-e, --ext [extension]', 'file extension used for generated JSX files. Defaults to jsx')
     .parse(process.argv);
 
 var jsonFileName = program.input || 'jsxjam.json'
 var jsonFilePath = path.resolve(process.env.PWD, jsonFileName)
 var outputDir = program.output || './'
-var outputDirPath = path.resolve(process.env.PWD, outputDir)
+var jsxExt = program.ext ? program.ext.replace('.','') : 'jsx'
+jsxExt = '.' + jsxExt
+
+var moduleComponents = {}
 
 // Run through the JSON
 jsonfile.readFile(jsonFilePath, function(err, obj) {
-    console.log(obj)
+    var modules = obj.modules || {}
+    var settings = obj.settings || {}
+    var baseDir = settings.baseDir || ''
+    for (var moduleName in modules) {
+        if (obj.modules.hasOwnProperty(moduleName)) {
+            var module = obj.modules[moduleName]
+            var components = module.components || []
+            components.forEach(function(componentName) {
+                var importPath = path.join('..', '..', moduleName, 'components', componentName)
+                moduleComponents[componentName] = {
+                    importPath: importPath,
+                    moduleName: moduleName,
+                }
+            })
+        }
+    }
+
+    for (var componentName in moduleComponents) {
+        if (moduleComponents.hasOwnProperty(componentName)) {
+            var component = moduleComponents[componentName]
+            var moduleName = component.moduleName
+            var filePath = path.join(process.env.PWD, outputDir, baseDir, moduleName, 'components', componentName + jsxExt)
+
+            var componentFile = ''
+                + "import React from 'react'"
+                + '\n' + "class " + componentName + " extends React.Component {"
+                + '\n' + "    render() {"
+                + '\n' + "        return <div className=\"" + componentName + "\">" + componentName + "</div>"
+                + '\n' + "    }"
+                + '\n' + "}"
+                + '\n'
+                + '\n' + "export default " + componentName
+
+            // TODO: use options to fsPath.removeSync(path.dirname(filePath))
+            fsPath.mkdirSync(path.dirname(filePath))
+            fsPath.writeFileSync(filePath, componentFile)
+            console.log(componentName, '-->', path.join(baseDir, moduleName, 'components', componentName + jsxExt))
+        }
+    }
+
 })
