0c1ef247a50a80393176110bc452bc20672f116a
Kevin Kirchner <kirchner.kevin@gmail.com>
2016-11-16 15:01:45 -0500

    installing and using commander

--- bin/jsxjam.js
+++ bin/jsxjam.js
@@ -1,29 +1,21 @@
 #! /usr/bin/env node
-console.log("=== Starting JSXjam session ===")
-
 var fs = require('fs')
 var path = require('path')
 var jsonfile = require('jsonfile')
+var program = require('commander')
 
-/* Options */
-// process.argv.forEach(function (val, index, array) {
-//   console.log(index + ': ' + val);
-// });
-const jsonFileName = process.argv[2] || 'jsxjam.json'
-const jsonFilePath = path.resolve(process.env.PWD, jsonFileName)
-const outputDir = process.argv[3] || process.env.PWD
-const outputDirPath = path.resolve(process.env.PWD, outputDir)
+program
+    .version('0.1.0')
+    .option('-i, --input [path]', 'path to the json file to parse. Defaults to jsxjam.json')
+    .option('-o, --output [path]', 'directory where you want your generated jsx files to end up. Defaults to ./')
+    .parse(process.argv);
 
-/* Settings */
-// TODO: find .jsxjamrc config file
-// TODO: override settings from command line options
-
-/* Build some JSX files */
-// TODO: find template path from settings/options
+var jsonFileName = program.input || 'jsxjam.json'
+var jsonFilePath = path.resolve(process.env.PWD, jsonFileName)
+var outputDir = program.output || './'
+var outputDirPath = path.resolve(process.env.PWD, outputDir)
 
 // Run through the JSON
 jsonfile.readFile(jsonFilePath, function(err, obj) {
     console.log(obj)
 })
-
-console.log("=== JSXjam session complete ===")
